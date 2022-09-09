00796eae617c8721bdc3bb1c40a739a0c924303a
vincent51689453 <vincent51689453@gmail.com>
2022-09-09 13:18:09 -0400

    update node-red dashboard

--- codebook.py
+++ codebook.py
@@ -7,7 +7,7 @@ topics_lib = ['wallet/info',
               'wallet/monthly_expense',
               'wallet/monthly_remain',
               'wallet/annual_remain',
-              'wallet/hsbc'
+              'wallet/total_assets'
 ]
 
 months = {'Jan':1,'Feb':2,'Mar':3,'Apr':4,'May':5,'Jun':6,'Jul':7,'Aug':8,'Sep':9,'Oct':10,'Nov':11,'Dec':12}
